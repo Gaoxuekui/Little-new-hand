fd22b86403c4e59f997948b25e14e917d068b95b
郜学魁 <88302619+Gaoxuekui@users.noreply.github.com>
2021-08-03 15:46:48 +0800

    Add files via upload

--- hello world30.c
+++ hello world30.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
 int main()
 {
-	long num = 100;
+	//long num = 100;
 	//int age = 20;
-	printf("%d\n",num);//%d--打印整型十进制的数据
+	//%c--打印字符
+	//%f--打印浮点数打小数
+	//printf("%d\n",num);//%d--打印整型十进制的数据
+	//float f = 5.0;
+	//printf("%f\n", f);
+	double d = 3.14;
+	printf("%lf\n", d);
 	return 0;
 }
